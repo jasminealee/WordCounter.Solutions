fbe074a5cfbd8efc9dcc185fad66e4a4586b61c8
Jasmine Lee <jasmine.al1722@gmail.com>
2019-02-15 15:26:25 -0800

    add more revisions to specs in WordCounter.Test

--- WordCounter.Tests/WordCounter.Tests.cs
+++ WordCounter.Tests/WordCounter.Tests.cs
@@ -1,37 +1,44 @@
-[TestMethod]
-public class FullWord;
+using Microsoft.TestTools;
+using System;
+using System.Collections.Generic;
+using WordCounter;
+
+namespace WordCounter.Tests
 {
-public void IsWordCounter_FullWordTrue()
+[TestMethod]
+public class MatchWordTest;
   {
+public void IsWordCounter_FullWordTrue()
+    {
   WordCounter testWordCounter = new WordCounter();
   Assert.AreEqual(true, testWordCounter.IsWordCounter(pen));
-  }
-}
+    }
 
 [TestMethod]
 public void IsWordCounter_PunctuationNotNecessaryTrue()
-{
+    {
   WordCounter testWordCounter = new WordCounter();
   Assert.AreEqual(true, testWordCounter.IsWordCounter(pen));
-}
+    }
 
 [TestMethod]
 public void IsWordCounter_NumberTrue()
-{
+    {
   WordCounter testWordCounter = new WordCounter();
   Assert.AreEqual(true, testWordCounter.IsWordCounter(pen));
-}
+    }
 
 [TestMethod]
 public void IsWordCounter_CapitalizationNotNecessary()
-{
+    {
   WordCounter testWordCounter = new WordCounter();
   Assert.AreEqual(true, testWordCounter.IsWordCounter(pen));
-}
+    }
 
 [TestMethod]
 public void IsWordCounter_NoSymbolsTrue()
-{
+    {
   WordCounter testWordCounter = new WordCounter();
   Assert.AreEqual(true, testWordCounter.IsWordCounter(pen));
+    }
 }
